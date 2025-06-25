bfd856cc08bb94c0856348a7734dad7e22960c39
douradobordeaux <douradobe@gmail.com>
2025-06-25 13:48:34 -0300

    sla
    sla

--- report_card.py
+++ report_card.py
@@ -1,4 +1,5 @@
 import json
+import rc_data
 class Subject:
     def __init__(self, name, grade, credits):
         self.name = name
@@ -7,12 +8,7 @@ class Subject:
         self.result = grade >= 5
 
     def subject_to_dictionary(self):
-        return {
-            "name": self.name,
-            "grade": self.grade,
-            "credits": self.credits,
-            "result": self.result
-        }
+        return {"name": self.name, "grade": self.grade, "credits": self.credits, "result": self.result}
     
     def subject_from_dictionary(data):
         subject = Subject(data["name"], data["grade"], data["credits"])
@@ -27,6 +23,14 @@ class Period:
     def insert_subject(self, subject):
         self.subjects.append(subject)
 
+    def period_to_dictionary(self):
+        return {"name": self.name, "subjects": [s.subject_to_dictionary() for s in self.subjects]}
+    
+    def period_from_dictionary(data):
+        period = Period(data["name"])
+        period.subjects = [Subject.subject_from_dictionary(s) for s in data["subjects"]]
+        return period
+    
     def calculate_period_average(self):
         subject_grades_weighted_sum = sum(s.grade * s.credits for s in self.subjects)
         subject_credits_sum = sum(s.credits for s in self.subjects)
@@ -40,25 +44,20 @@ class Period:
 
     def calculate_period_fails(self):
         return sum(1 for s in self.subjects if not s.result)
-    
-    def period_to_dictionary(self):
-        return {
-            "name": self.name,
-            "subjects": [s.subject_to_dictionary() for s in self.subjects]
-        }
-    
-    def period_from_dictionary(data):
-        period = Period(data["name"])
-        period.subjects = [Subject.subject_from_dictionary(s) for s in data["subjects"]]
-        return period
+
 
 class ReportCard:
-
     def __init__(self):
         self.periods = []
     
     def insert_period(self, period):
         self.periods.append(period)
+
+    def reportcard_to_dictionary(self):
+        return {"periods" : [p.period_to_dictionary() for p in self.periods]}
+    
+    def reportcard_from_dictionary(data):
+        report_card.periods = []
 
     def calculate_current_total_average(self, period):
         period_grades_weighted_sum = sum(p.calculate_period_average() * p.calculate_period_credits() for p in self.periods[:period])
@@ -74,19 +73,19 @@ class ReportCard:
     def calculate_current_total_fails(self, period):
         return sum(p.calculate_period_fails() for p in self.periods[:period])
     
-    def register_report_card(self):
-        periods_amount = int(input("Type how many periods you want to register: "))
-        for p in range (periods_amount):
-            period_name = input("Type the name of the period you want to register: ")
-            period = Period(period_name)
-            subjects_amount = int(input("Type how many subjects you want to register: "))
-            for s in range (subjects_amount):
-                subject_name = input("Type the name of the subject you want to register: ")
-                subject_grade = float(input("Type the grade of the subject you want to register: "))
-                subject_credits = int(input("Type the credits of the subject you want to register: "))
-                subject = Subject(subject_name, subject_grade, subject_credits)
-                period.insert_subject(subject)
-            self.insert_period(period)
+    # def register_report_card(self):
+        # periods_amount = int(input("Type how many periods you want to register: "))
+        # for p in range (periods_amount):
+        #     period_name = input("Type the name of the period you want to register: ")
+        #     period = Period(period_name)
+        #     subjects_amount = int(input("Type how many subjects you want to register: "))
+        #     for s in range (subjects_amount):
+        #         subject_name = input("Type the name of the subject you want to register: ")
+        #         subject_grade = float(input("Type the grade of the subject you want to register: "))
+        #         subject_credits = int(input("Type the credits of the subject you want to register: "))
+        #         subject = Subject(subject_name, subject_grade, subject_credits)
+        #         period.insert_subject(subject)
+        #     self.insert_period(period)
         
     def print_report_card(self):
         print("\n\n")
@@ -106,6 +105,7 @@ class ReportCard:
             print(f"Period Average: {period_average:.2f} / Period Earned Credits: {period_earned_credits} / Period Credits: {period_credits} / Period Fails: {period_fails}")
             print(f"Total Average: {total_average:.2f} / Total Earned Credits: {total_earned_credits} / Total Credits: {total_credits} / Total Fails: {total_fails}")
             print("\n====================")
+
 
 report_card = ReportCard()
 report_card.register_report_card()
